f744615dd5911c3ce33cd041506a32010c316039
Oscar <oscar.carrasco1980@gmail.com>
2019-04-25 21:55:02 -0400

    Aplicado el buscador de Heroes

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -59,8 +59,24 @@ export class HeroesService {
         console.log("Servicio listo para usar.");
     }
 
-    getHeroes(){
+    getHeroes():Heroe[]{
         return this.heroes;
+    }
+
+    getHeroe( idx: string){
+      return this.heroes[idx];
+    }
+
+    buscarHeroes( termino:string): Heroe[]{
+      let heroesArr: Heroe[] =[];
+      termino = termino.toLowerCase();
+      for ( let heroe of this.heroes ) {
+        let nombre = heroe.nombre.toLocaleLowerCase();
+        if ( nombre.indexOf( termino) >= 0 ){
+          heroesArr.push(heroe);
+        }
+      }
+      return heroesArr;
     }
 }
 
